44d90e0b677ab6b08b4d9875e34b5f792160b2de
Md. Abuhorayra <64754052+Horayra077@users.noreply.github.com>
2021-12-11 00:37:32 +0600

    Add files via upload

---
+++ includes/widget.php
@@ -0,0 +1,13 @@
+                <div class="well">
+                    <img class="img-responsive" src="images/<?php echo '../images/my_self.jpg'; ?>" alt="">
+                    <h4>ABOUT HORAYRA</h4>
+                    <h3>Hi, I'm Horayra, I'm a Software Engineer! I did my graduation in Computer science and engineering from East-West University.</h3>
+                </div>
+
+
+
+<!--
+                <div class="well">
+                    <h4>Side Widget Well</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+                </div>-->
