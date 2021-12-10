f0ea28a5f2e02805295719069ed932a55cfcaa95
Md. Abuhorayra <64754052+Horayra077@users.noreply.github.com>
2021-12-11 00:33:16 +0600

    Add files via upload

---
+++ admin/categories.php
@@ -0,0 +1,112 @@
+<?php include "includes/admin_header.php" ?>
+
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+
+        
+    
+<?php include "includes/admin_navigation.php" ?>
+        
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        
+                        
+                        <h1 class="page-header">
+                            Welcome to admin
+                            <small>Author</small>
+                        </h1>
+                        
+                        
+                        
+                        <!-- Add Category --> 
+                        
+                        <div class="col-xs-6">
+                            
+<?php insert_categories(); ?>
+
+                            
+                        <form action="" method="post">
+                            <div class="form-group">
+                                  <lable for="cat-title">Add Category</lable> 
+                                <input type="text" class="form-control" name="cat_title">
+                            </div>
+                            <div class="form-group">
+                                <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
+                            </div>
+                            
+                        </form>
+                       
+
+                        
+                        <?php
+                            
+                        //Update and Include Query
+                            
+                        if(isset($_GET['edit']))
+                        {
+                            $cat_id = $_GET['edit'];
+                            
+                            include "includes/update_categories.php"; 
+                        }
+                            
+                        
+                        ?>
+                        
+                        
+                        
+                        </div> 
+                        
+                        
+                        
+                         <!-- Display Category --> 
+                        
+                        <div class="col-xs-6">     
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Category Title</th>
+                                    <th><span class="glyphicon glyphicon-trash"></span> Delete</th>
+                                    <th><span class="glyphicon glyphicon-edit"></span> Edit</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+<!-- FIND ALL CATEGORY QUERY-->
+   <?php   findAllCategories(); ?> 
+
+<!-- DELETE CATEGORY QUERY -->
+   <?php   deleteCategories();  ?> 
+                                
+                                
+
+                                
+                                
+                            </tbody>
+                        </table>
+                        </div>
+                        
+                        
+
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+        
+        
+        
+<?php include "includes/admin_footer.php" ?>
+
